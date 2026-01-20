d4bbfa69f4b90c553cd9191105c7291235e11a88
Thelmina4 <thelma.clarke24@mail.dcu.ie>
2026-01-20 14:01:31 +0000

    adapt to in class notes

--- lab1/firstdjango/urls.py
+++ lab1/firstdjango/urls.py
@@ -5,7 +5,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="index"),    # homepage ofr 0.0.0.0:0000
+    path('', views.home, name="home"),    # homepage ofr 0.0.0.0:0000
     path('contact/', views.contact, name="contact"),
     path('about/', views.about, name="about"),
     path("profile/<int:id>/", views.profile, name="profile"), # profile/1), profile/2
